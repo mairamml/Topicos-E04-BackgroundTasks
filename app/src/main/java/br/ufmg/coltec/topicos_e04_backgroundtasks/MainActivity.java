df6b0c2af66938724025db9ccaeb9cd56ebc59fb
Maíra Amaral <66277650+mairamml@users.noreply.github.com>
2024-10-07 16:42:42 -0300

    Done!

--- app/src/main/java/br/ufmg/coltec/topicos_e04_backgroundtasks/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/topicos_e04_backgroundtasks/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -21,14 +23,39 @@ public class MainActivity extends AppCompatActivity {
         Button downloadBtn = findViewById(R.id.btn_download);
         EditText txtLink = findViewById(R.id.txt_img_link);
         ImageView imgView = findViewById(R.id.img_picture);
+        ProgressBar progress = findViewById(R.id.progress);
 
 //        TODO[1]: Processo de download e carregamento da imagem acontecendo na Main Thread, ALTERAR!!
         downloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO[2]: Exibir barra de progresso quando estiver fazendo download da imagem
-                Bitmap img = MainActivity.this.downloadImage(txtLink.getText().toString());
-                imgView.setImageBitmap(img);
+                progress.setVisibility(View.VISIBLE);
+                imgView.setVisibility(View.GONE);
+                String url = txtLink.getText().toString();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //para executar em background
+                        Bitmap img = null;
+                        try {
+                            img = ImageDownloader.download(url);
+                            if(img != null){
+                                imgView.setImageBitmap(img);
+                                imgView.setVisibility(View.VISIBLE);
+                                progress.setVisibility(View.GONE);
+                            }else{
+                                Log.e("MainActivity", "Image couldn't be loaded");
+                            }
+                        } catch (IOException e) {
+
+                            runOnUiThread(() -> {
+                                progress.setVisibility(View.GONE);
+                            });
+                        }
+                    }
+                }).start();
             }
         });
     }
